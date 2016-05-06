92f6006ba0829cf513b815d122f606416f1c038a
Jackeagle <jackeagle102@gmail.com>
2016-05-06 09:29:01 +0530

    Samsung stock SM-J510FN_SWA_MM_Opensource.zip
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

---
+++ build_kernel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export ARCH=arm
+export CROSS_COMPILE=$(pwd)/arm-eabi-4.8/bin/arm-eabi-
+mkdir output
+
+make -C $(pwd) O=output msm8916_sec_defconfig VARIANT_DEFCONFIG=msm8916_sec_j5xlte_eur_defconfig SELINUX_DEFCONFIG=selinux_defconfig
+make -C $(pwd) O=output
+
+cp output/arch/arm/boot/Image $(pwd)/arch/arm/boot/zImage
